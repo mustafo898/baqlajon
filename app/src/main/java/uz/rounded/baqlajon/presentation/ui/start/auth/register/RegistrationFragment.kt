a5a7e76e30407baf7d9580df5914243849416082
musta <mustafoabdugafurov55@gmail.com>
2023-02-11 18:51:30 +0500

    sms

--- app/src/main/java/uz/rounded/baqlajon/presentation/ui/start/auth/register/RegistrationFragment.kt
+++ app/src/main/java/uz/rounded/baqlajon/presentation/ui/start/auth/register/RegistrationFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dagger.hilt.android.AndroidEntryPoint
+import uz.rounded.baqlajon.R
+import uz.rounded.baqlajon.core.extensions.navigate
 import uz.rounded.baqlajon.core.extensions.popBackStack
 import uz.rounded.baqlajon.databinding.FragmentRegistrationBinding
 import uz.rounded.baqlajon.presentation.MainActivity
@@ -22,13 +24,11 @@ class RegistrationFragment : BaseFragment<FragmentRegistrationBinding>() {
     override fun created(view: View, savedInstanceState: Bundle?) {
 
         binding.signUp.cardView.setOnClickListener {
-            startActivity(Intent(requireContext(), MainActivity::class.java))
-            (activity as StartActivity).finish()
+            navigate(R.id.action_registrationFragment_to_smsVerifyFragment)
         }
 
         binding.login.setOnClickListener {
             popBackStack()
         }
     }
-
 }
