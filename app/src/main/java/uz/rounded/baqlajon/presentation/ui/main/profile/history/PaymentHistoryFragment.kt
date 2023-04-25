041e795b25cd29ea7f554b3c61cb445b17dc7347
SuhrobDev <suhrobqayumov4@gmail.com>
2023-04-25 20:57:46 +0500

    small changes

--- app/src/main/java/uz/rounded/baqlajon/presentation/ui/main/profile/history/PaymentHistoryFragment.kt
+++ app/src/main/java/uz/rounded/baqlajon/presentation/ui/main/profile/history/PaymentHistoryFragment.kt
@@ -5,8 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import uz.rounded.baqlajon.databinding.FragmentPaymentHistoryBinding
-import uz.rounded.baqlajon.presentation.ui.main.profile.history.adapter.PaymentHistoryAdapter
 import uz.rounded.baqlajon.presentation.ui.BaseFragment
+import uz.rounded.baqlajon.presentation.ui.main.profile.history.adapter.PaymentHistoryAdapter
 
 class PaymentHistoryFragment : BaseFragment<FragmentPaymentHistoryBinding>() {
     override fun createBinding(
@@ -23,7 +23,7 @@ class PaymentHistoryFragment : BaseFragment<FragmentPaymentHistoryBinding>() {
 
     override fun created(view: View, savedInstanceState: Bundle?) {
         binding.list.adapter = adapter
-
+        hideMainProgress()
         val list = mutableListOf<String>()
         for (i in 0 until 10) {
             list.add("")
